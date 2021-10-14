3e28df1bba34babb81d9a0388de79031b52be5ad
jlau0019 <jlau0019@student.monash.edu>
2021-10-14 08:16:49 +0800

    Created index.html and layout

---
+++ js/vega_vis.js
@@ -0,0 +1,19 @@
+var vg_1 = "diabetesPatientsGender.vg.json";
+vegaEmbed("#gender", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "map.vg.json";
+vegaEmbed("#change", vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "diabetesIsopleth.vg.json";
+vegaEmbed("#isopleth", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "causesOfDeath.vg.json";
+vegaEmbed("#death", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "diabetesPrediction.vg.json";
+vegaEmbed("#predict", vg_5).then(function(result) {
+}).catch(console.error);
