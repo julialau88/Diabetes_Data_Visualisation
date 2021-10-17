99c3f14a6ae25e8565c4de518300ce4b0cab6264
jlau0019 <jlau0019@student.monash.edu>
2021-10-17 21:46:38 +0800

    removed action menu

--- js/vega_vis.js
+++ js/vega_vis.js
@@ -1,5 +1,5 @@
 var vg_1 = "diabetesPatientsGender.vg.json";
-vegaEmbed("#gender", vg_1).then(function(result) {
+vegaEmbed("#gender", vg_1, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "map.vg.json";
@@ -7,13 +7,13 @@ vegaEmbed("#change", vg_2).then(function(result) {
 }).catch(console.error);
 
 var vg_3 = "diabetesIsopleth.vg.json";
-vegaEmbed("#isopleth", vg_3).then(function(result) {
+vegaEmbed("#isopleth", vg_3, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "causesOfDeath.vg.json";
-vegaEmbed("#death", vg_4).then(function(result) {
+vegaEmbed("#death", vg_4, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_5 = "diabetesPrediction.vg.json";
-vegaEmbed("#predict", vg_5).then(function(result) {
+vegaEmbed("#predict", vg_5, {"actions": false}).then(function(result) {
 }).catch(console.error);
